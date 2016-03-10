1c29a4c14e94581ee71e46b2cb5b79f42d213199
JerryCh0 <tkachenko.da@phystech.edu>
2016-03-10 16:02:19 +0300

    Свой printf.

---
+++ printf/printf/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  printf
+//
+//  Created by Дмитрий Ткаченко on 09/03/16.
+//  Copyright © 2016 Dmitry Tkachenko. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
