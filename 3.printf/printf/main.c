2524b3a047a753c1c850215d17c67c8a03e839b0
Vladislav27 <27vl@mail.ru>
2016-04-08 21:26:14 +0300

    Update main.c

--- 3.printf/printf/main.c
+++ 3.printf/printf/main.c
@@ -1,15 +1,36 @@
-//
-//  main.c
-//  printf
-//
-//  Created by Дмитрий Ткаченко on 09/03/16.
-//  Copyright © 2016 Dmitry Tkachenko. All rights reserved.
-//
+#include<stdarg.h>
+#include<string.h>
+#include<stdio.h>
 
-#include <stdio.h>
+void my_printf(char* s, ...)
+{
+	va_list L;
+	va_start(L, s);
+	while (*s != '\0')
+	{
+		if (*s != '%') printf("%c", *s);
+		if (*s == '%') {
+			s++;
+			switch (*s) {
+			case 's':
+				printf("%s", va_arg(L, char *));
+				break;
+			case 'd':
+				printf("%d", atoi(va_arg(L, char *)));
+				break;
+			}
+			s++;
+		}
+	}
+	va_end(L);
+}
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
-    return 0;
+
+
+void main()
+{
+	char a[250];
+	int len = 0;
+	gets(a);
+	my_printf("%d", a);
 }
