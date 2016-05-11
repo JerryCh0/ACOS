c3acc0300b29c815627d8c589a0a31bf48576f33
JerryCh0 <tkachenko.da@phystech.edu>
2016-05-11 16:08:06 +0300

    Finish FIFO

--- FIFO/chat_thread_duplex.cpp
+++ FIFO/chat_thread_duplex.cpp
@@ -15,36 +15,44 @@
 
 using namespace std;
 
-//Constant names.
+//Имена семафоров.
 const char* client_read_name = "/clientcanread";
 const char* server_read_name = "/servercanread";
 const char* fifo_busy_name = "/chatfifobusy";
+//Имя FIFO.
 const char* fifo_name = "chatfifo";
 
-//Creating semaphores.
+//Создаем семафоры для клиента, сервера, FIFO.
 sem_t *client_can_read, *server_can_read, *fifo_busy;
 
 //pthread_create
 //Первый параметр этой функции представляет собой указатель на переменную типа pthread_t, которая служит идентификатором создаваемого потока. Второй параметр, указатель на переменную типа pthread_attr_t, используется для передачи атрибутов потока. Третьим параметром функции pthread_create() должен быть адрес функции потока. Эта функция играет для потока ту же роль, что функция main() – для главной программы. Четвертый параметр функции pthread_create() имеет тип void *. Этот параметр может использоваться для передачи значения, возвращаемого функцией потока. Вскоре после вызова pthread_create() функция потока будет запущена на выполнение параллельно с другими потоками программы.
 
-//Sending data.
+//Запись сообщения в FIFO.
+//Функция write(handle, buf, count) переписывает count байт из буфера, на который указывает buf в файл, соот­ветствующий дескриптору файла handle.
 void SendData(int fout, const string& str) {
     int len = str.length();
+    //Записываем так же длину сообщения, чтобы его можно было корректно считать из FIFO.
     write(fout, &len, sizeof(int));
     write(fout, str.c_str(), str.length() * sizeof(char));
 }
 
-//Receiving data.
+//Считывание сообщения из FIFO.
+//Функция read(fd, buf, count) считывает count байт из файла, описываемого аргументом fd, в буфер, на который указывает аргумент buf.
 void ReceiveData(int fin, string& str) {
     int len = 0;
+    //Cчитываем размер сообщения.
     read(fin, &len, sizeof(int));
+    //Создаем буфер для самого сообщения.
     char *buf = new char[len];
+    //Считываем сообщение в буфер.
     read(fin, buf, len);
     str = string(buf);
+    //Удаляем буфер.
     delete buf;
 }
 
-//Checking semaphore.
+//Проверяем, корректно ли открыт семафор.
 void check_sem_open(sem_t* semaphore) {
     if (semaphore == SEM_FAILED) {
         cout << strerror(errno) << endl;;
@@ -54,11 +62,16 @@ void check_sem_open(sem_t* semaphore) {
 
 //Pending data.
 void* PendingData(void* data) {
+    //Открываем FIFO для чтения и записи.
     int fin = open(fifo_name, O_RDWR);
+    //Создаем флаг типа запуска.
     bool is_server = *((bool*)data);
-    cout << "Ready :) Pending" << endl;
+    //Уведомление об ожидании ввода.
+    cout << "Ready, pending." << endl;
+    //Инициализируем строку, в которую будем получать информацию.
     string str;
-    while (str != "Goodbye") {
+    //Пока строка не совпадает с командой закрытия "exit", захватываем нужный семафор (в зависимости от типа).
+    while (str != "exit") {
         if (is_server) {
             if (sem_wait(server_can_read) < 0) {
                 perror("sem_wait");
@@ -69,12 +82,13 @@ void* PendingData(void* data) {
                 perror("sem_wait");
             }
         }
-        
+        //Захватываем семафор FIFO.
         sem_wait(fifo_busy);
-
+        //Получаем информацию.
         ReceiveData(fin, str);
-        cout << "He : " << str << endl;
-
+        //Выводим сообщение.
+        cout << "The other chatmate: " << str << endl;
+        //Освобождаем семафор FIFO.
         sem_post(fifo_busy);
     }
 
@@ -84,31 +98,36 @@ void* PendingData(void* data) {
         sem_close(client_can_read);
     sem_close(fifo_busy);*/
     
+    //Закрываем FIFO, возвращем пустое значение.
     close(fin);
     return NULL;
 }
 
 //Input data.
 void* UserInput(void* data) {
+    //Открываем FIFO для чтения и записи.
     int fout = open(fifo_name, O_RDWR);
+    //Создаем флаг типа запуска.
     bool is_server = *((bool*)data);
-
-    cout << "Ready :) UserInput" << endl;
+    //Уведомление об ожидании ввода.
+    cout << "Ready, Input." << endl;
+    //Инициализируем строку, в которую будем получать информацию.
     string str;
-    while (str != "Goodbye") {
+    while (str != "exit") {
+        //Считываем из стандартного потока сообщение.
         getline(cin, str);
-        
+        //Захватываем семафор FIFO.
         sem_wait(fifo_busy);
-
+        //Отправляем в FIFO сообщение.
         SendData(fout, str);
-
+        //Освобождаем семафор в зависимости от типа запуска.
         if (is_server) {
             sem_post(client_can_read);
         }
         else {
             sem_post(server_can_read);
         }
-        
+        //Освобождаем семафор FIFO.
         sem_post(fifo_busy);
     }
     
@@ -118,6 +137,7 @@ void* UserInput(void* data) {
         sem_close(server_can_read);
     sem_close(fifo_busy);*/
     
+    //Закрываем FIFO, возвращаем пустое значение.
     close(fout);
     return NULL;
 }
@@ -127,22 +147,30 @@ void Server() {
     mkfifo(fifo_name, 0777); //as usual file
     //Создаем дескриптор потока.
     pthread_t temp;
-    //Инициализируем указатель на bool - тип текущего запуска сервером является сервером.
+    //Инициализируем указатель на bool - тип текущего запуска является сервером.
     bool* is_server = new bool;
     *is_server = true;
-    //
+    //Создаем поток с атрибутами по умолчанию, выполняющий функцию PendingData, принимающую аргумент is_server.
     pthread_create(&temp, NULL, PendingData, is_server);
+    //Ввод информации пользователем.
     UserInput(is_server);
+    //Удаление потока управления.
     pthread_cancel(temp);
 }
 
 void Client() {
+    //Создаем FIFO с максимальными правами.
     mkfifo(fifo_name, 0777); //as usual file
+    //Создаем дескриптор потока.
     pthread_t temp;
+    //Инициализируем указатель на bool - тип текущего запуска не является сервером (является клиентом).
     bool* is_server = new bool;
     *is_server = false;
+    //Создаем поток с атрибутами по умолчанию, выполняющий функцию PendingData, принимающую аргумент is_server.
     pthread_create(&temp, NULL, PendingData, is_server);
+    //Ввод информации пользователем.
     UserInput(is_server);
+    //Удаление потока управления.
     pthread_cancel(temp);
 }
 
@@ -185,7 +213,7 @@ int main(int argc, char **argv) {
         Client();
     }
     else {
-        puts("Unknow argument\n");
+        puts("Unknown argument\n");
         exit(-1);
     }
     
